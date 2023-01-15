bb62a1e88641a91a0f2acc16c523c47dd4f39664
Robert Romero <robert02frontend@gmail.com>
2023-01-14 22:24:26 -0400

    Initial Commit

---
+++ Myfirstapp/MyfirstappApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyfirstappApp.swift
+//  Myfirstapp
+//
+//  Created by Robert Romero Medina on 14/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MyfirstappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
