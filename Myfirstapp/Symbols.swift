99c09c8dbd062e2c968b8f36f75f18ccef10c455
Robert Romero <robert02frontend@gmail.com>
2023-01-15 09:49:27 -0400

    SF Symbols

---
+++ Myfirstapp/Symbols.swift
@@ -0,0 +1,31 @@
+//
+//  Symbols.swift
+//  Myfirstapp
+//
+//  Created by Robert Romero Medina on 15/1/23.
+//
+
+import SwiftUI
+
+struct Symbols: View {
+    var body: some View {
+        Image(systemName: "moon.circle.fill")
+            .renderingmode(.original)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 200)
+//            .foregroundColor(.green)
+        
+//        Text("Sigueme en Twitter")
+//            .underline()
+//            .font(.largeTitle)
+//            .bold()
+//            .padding()
+    }
+}
+
+struct Symbols_Previews: PreviewProvider {
+    static var previews: some View {
+        Symbols()
+    }
+}
