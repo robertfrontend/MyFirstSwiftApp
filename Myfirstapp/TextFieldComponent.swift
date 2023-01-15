5e781a1584f75588ed0de1c3ad97025226f2a3d0
Robert Romero <robert02frontend@gmail.com>
2023-01-15 12:03:21 -0400

    editor and state color

--- Myfirstapp/TextFieldComponent.swift
+++ Myfirstapp/TextFieldComponent.swift
@@ -12,44 +12,76 @@ struct TextFieldComponent: View {
     @State var username: String = ""
     @State var password: String = ""
     
+    @State var text = "Escribe algo..."
+    @State var counter = 0
+    
     var body: some View {
         VStack {
-            TextField("Username", text: $username)
-                // input
-                .keyboardType(.emailAddress)
-                .disableAutocorrection(true)
-            
-                // styles
-                .padding(8)
-                .font(.headline)
-                .background(.gray.opacity(0.3))
-                .cornerRadius(6)
-                .padding(.horizontal, 60)
-                .padding(.top, 40)
+            VStack {
+                Text("TextField")
+                    .font(.largeTitle)
+                    .bold()
+                Text("🚀Tu nombre de usuario es:")
+                Text("\(username)")
+                    .bold()
+                TextField("Username", text: $username)
+                    // input
+                    .keyboardType(.emailAddress)
+                    .disableAutocorrection(true)
                 
-                .onChange(of: username, perform: {value in
-                    print("New email \(value)")
-                })
-            SecureField("Password", text: $password)
-                // input
-                .keyboardType(.default)
-                .disableAutocorrection(true)
-                .autocapitalization(.none)
-            
-                // styles
-                .padding(8)
-                .font(.headline)
-                .background(.gray.opacity(0.3))
-                .cornerRadius(6)
-                .padding(.horizontal, 60)
+                    // styles
+                    .padding(8)
+                    .font(.headline)
+                    .background(.gray.opacity(0.3))
+                    .cornerRadius(6)
+                    .padding(.horizontal, 60)
+                    .padding(.top, 40)
+                    
+                    .onChange(of: username, perform: {value in
+                        print("New email \(value)")
+                    })
+                SecureField("Password", text: $password)
+                    // input
+                    .keyboardType(.default)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
+                
+                    // styles
+                    .padding(8)
+                    .font(.headline)
+                    .background(.gray.opacity(0.3))
+                    .cornerRadius(6)
+                    .padding(.horizontal, 60)
 
-                .onChange(of: password, perform: {value in
-                    print("New password \(value)")
-                })
+                    .onChange(of: password, perform: {value in
+                        print("New password \(value)")
+                    })
+            }
+            VStack {
+                Text("Text editor")
+                    .font(.largeTitle)
+                    .bold()
+                
+                TextEditor(text: $text)
+                    .font(.title)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+                    .foregroundColor(.blue)
+                    .padding(1)
+                    .onChange(of: text, perform: {value in
+                        counter = value.count
+                    })
+                
+                Text("\(counter)")
+                    .foregroundColor(counter <= 280 ? .green : .red)
+                    .font(.largeTitle)
+                
+            }
+
             Spacer()
             
+            
         }.padding()
-//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
