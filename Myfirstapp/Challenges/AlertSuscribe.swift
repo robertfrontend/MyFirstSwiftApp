048deaa12337ea39af48d9c6d3e4aef66a08fd5a
Robert Romero <robert02frontend@gmail.com>
2023-01-15 14:24:03 -0400

    agregandole estado al challenge 1

--- Myfirstapp/Challenges/AlertSuscribe.swift
+++ Myfirstapp/Challenges/AlertSuscribe.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct AlertSuscribe: View {
+    
+    @State var like:Bool = true
+    @State var activeBell = false
+    @State var isSuscriptor = false
+    
     var body: some View {
         // Reto 1
         VStack {
@@ -16,41 +21,54 @@ struct AlertSuscribe: View {
                 .bold()
                 .foregroundColor(.blue)
             VStack {
-                
                 HStack {
                     VStack {
                         HStack {
                             // like
-                            Image(systemName: "hand.thumbsup.fill")
-                                .scaledToFit()
-                                .foregroundColor(.blue)
-                                .font(.system(size: 40))
-                          
+                            Button(action: {
+                                like = true
+                            }, label: {
+                                Image(systemName: "hand.thumbsup.fill")
+                                    .scaledToFit()
+                                    .foregroundColor(like ? .blue : .black)
+                                    .font(.system(size: 40))
+                            })
                             // dislike
-                            Image(systemName: "hand.thumbsdown.fill")
-                                .scaledToFit()
-                                .foregroundColor(Color(hue: 1.0, saturation: 0.0, brightness: 0.20))
-                                .font(.system(size: 40))
+                            Button(action: {
+                             like = false
+                            }, label: {
+                                Image(systemName: "hand.thumbsdown.fill")
+                                    .scaledToFit()
+                                    .foregroundColor(!like ? .blue : .black)
+                                    .font(.system(size: 40))
+                            })
                         }
                         VStack {
-                        }.frame(width: 100, height: 5) .background(.blue)
+                        }.frame(width: 100, height: 5) .background(like ? .blue : .black)
                     }.padding()
                     VStack {
                         // button
                         HStack {
-                            Button(action: {print("Nincee")}, label: {
-                                Text("Suscribete")
+                            Button(action: {
+                                isSuscriptor = !isSuscriptor
+                            }, label: {
+                                Text(isSuscriptor ? "Suscribed" : "Join Sus")
                                     .bold()
                                     .font(.title2)
                                     .foregroundColor(.white)
                                     .padding(.all, 10)
-                                    .background(Color(hue: 0.958, saturation: 0.0, brightness: 0.36))
+                                    .background(!isSuscriptor ? .red : Color(hue: 0.958, saturation: 0.0, brightness: 0.36))
                                     .cornerRadius(10)
                             })
-                            Image(systemName: "bell.and.waves.left.and.right.fill")
-                                .scaledToFit()
-                                .foregroundColor(Color(hue: 1.0, saturation: 0.0, brightness: 0.20))
-                                .font(.system(size: 30))
+                            Button(action: {
+                                activeBell = !activeBell
+                            }, label: {
+                                Image(systemName: activeBell ? "bell.and.waves.left.and.right.fill" : "bell")
+                                    .scaledToFit()
+                                    .foregroundColor(Color(hue: 1.0, saturation: 0.0, brightness: 0.20))
+                                    .font(.system(size: 30))
+
+                            })
                         }
                     }
                 }
